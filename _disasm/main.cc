39360224d36e65d09346677afde6db8fc2472879
Daniel Prilik <danielprilik@gmail.com>
2016-12-29 11:28:41 -0500

    added some labels + in-program info re RELs, ESRs, and ESDs

--- _disasm/main.cc
+++ _disasm/main.cc
@@ -1,9 +1,10 @@
 #include <iostream>
-#include <sstream>
 #include <iomanip>
+#include <sstream>
 #include <fstream>
 
-#include <cstring>
+#include <vector>
+
 #include <cstdint>
 
 #include "disasm.h"
@@ -12,7 +13,7 @@
 
 using namespace std;
 
-char swap(char & a, char & b) {
+void swap(char & a, char & b) {
   char tmp = a;
   a = b;
   b = tmp;
@@ -44,21 +45,23 @@ int main(int argc, char const *argv[]) {
   if (argc != 2) {
     cerr 
       << "Usage: ./disasm <filename>" 
-      << endl;
+      ;
     return -1;
   }
+
+  vector<string> outLines;
 
   // Load program
   ifstream bin (argv[1], ifstream::binary);
 
   // Check for IO error
   if (!bin.is_open()) {
-    cerr << "Cannot open file." << endl;
+    cerr << "Cannot open file." ;
     return -1;
   }
 
-  // This var is also used later to load in user data after program!
-  uint32_t memaddr = 0x0;
+
+  // Disasm
   uint32_t word;
 
   bool isMERL = false;
@@ -66,36 +69,37 @@ int main(int argc, char const *argv[]) {
   int codeLength;
   
   for ever {
-    try {
-      word = readMIPSbyte(bin);
-    } catch (const string & msg) {
+    stringstream outLn;
+
+    try { word = readMIPSbyte(bin); } 
+    catch (const string & msg) {
       if (msg == "EOF") break;
     }
 
-    if (memaddr == 0x0 && word == 0x10000002) {
+    if (outLines.size() == 0 && word == 0x10000002) {
       isMERL = true;
-      cout 
+      outLn 
         << ".word 0x10000002" 
-        << " ; MERL cookie" 
-        << endl;
+        << " ; MERL cookie";
+      outLines.push_back(outLn.str());
     }
-    else if (memaddr == 0x4 && isMERL) {
+    else if (outLines.size() == 1 && isMERL) {
       fileLength = (int)word;
-      cout 
+      outLn 
         << ".word " << toHex(word)
         << " ; File Length (in bytes) is "
-        << fileLength
-        << endl;
+        << fileLength;
+      outLines.push_back(outLn.str());
     }
-    else if (memaddr == 0x8 && isMERL) {
+    else if (outLines.size() == 2 && isMERL) {
       codeLength = (int)word;
-      cout 
+      outLn 
         << ".word " << toHex(word)
         << " ; Code Length (in bytes) is "
-        << codeLength
-        << endl;
+        << codeLength;
+      outLines.push_back(outLn.str());
     }
-    else if (isMERL && memaddr >= codeLength) {
+    else if (isMERL && outLines.size() * 4 >= codeLength) {
       if (word == 0x01) {
         // Relocation Table
         uint32_t addr2relocate;
@@ -110,21 +114,26 @@ int main(int argc, char const *argv[]) {
           }
         }
 
-        cout 
-          << ".word 0x01       ; REL" 
-          << endl;
-        cout << ".word " << toHex(addr2relocate) 
-          << " ; === ln " << (int)(addr2relocate / 4 + 1)
-          << endl;
+        outLn
+          << ".word 0x01       ; REL";
+        outLines.push_back(outLn.str());
+
+        outLn = stringstream(); // clear stringstream
+        outLn
+          << ".word " << toHex(addr2relocate) 
+          << " ; === ln " << (int)(addr2relocate / 4 + 1);
+        outLines.push_back(outLn.str());
+
+        outLines[(int)(addr2relocate / 4)] += " ; REL'd";
       }
 
       if (word == 0x05) {
         // ESD
-        uint32_t addr2import;
+        uint32_t addr2export;
         int   name_len;
         char* name;
         try { 
-          addr2import = readMIPSbyte(bin);
+          addr2export = readMIPSbyte(bin);
           name_len    = readMIPSbyte(bin);
 
           name = new char [name_len + 1];
@@ -143,31 +152,39 @@ int main(int argc, char const *argv[]) {
           }
         }
 
-        cout 
+        outLn 
           << ".word 0x05       ; ESD - "
-          << name
-          << endl;
-        cout 
-          << ".word " << toHex(addr2import) 
-          << " ; === ln " << (int)(addr2import / 4 + 1)
-          << endl;
+          << name;
+        outLines.push_back(outLn.str());
+
+        outLn = stringstream(); // clear stringstream
+        outLn 
+          << ".word " << toHex(addr2export) 
+          << " ; === ln " << (int)(addr2export / 4 + 1);
+        outLines.push_back(outLn.str());
+
+
         for (int i = 0; i < name_len; i++) {
-          cout 
+          outLn = stringstream(); // clear stringstream
+          outLn 
             << ".word " << setw(3) << left << (int)name[i] 
-            << " ; " << name[i] 
-            << endl;
+            << " ; " << name[i];
+          outLines.push_back(outLn.str());
         }
+
+        int esdLine = (int)(addr2export / 4);
+        outLines[esdLine] = string(name) + ":\n" + outLines[esdLine];
 
         delete [] name;
       }
 
       if (word == 0x11) {
         // ESR
-        uint32_t addr2export;
+        uint32_t addr2import;
         int   name_len;
         char* name;
         try { 
-          addr2export = readMIPSbyte(bin);
+          addr2import = readMIPSbyte(bin);
           name_len    = readMIPSbyte(bin);
 
           name = new char [name_len + 1];
@@ -186,28 +203,60 @@ int main(int argc, char const *argv[]) {
           }
         }
 
-        cout 
+        outLn 
           << ".word 0x11       ; ESR - "
-          << name
-          << endl;
-        cout 
-          << ".word " << toHex(addr2export) 
-          << " ; === ln " << (int)(addr2export / 4 + 1)
-          << endl;
+          << name;
+        outLines.push_back(outLn.str());
+
+        outLn = stringstream(); // clear stringstream
+        outLn 
+          << ".word " << toHex(addr2import) 
+          << " ; === ln " << (int)(addr2import / 4 + 1);
+        outLines.push_back(outLn.str());
+
         for (int i = 0; i < name_len; i++) {
-          cout 
+          outLn = stringstream(); // clear stringstream
+          outLn 
             << ".word " << setw(3) << left << (int)name[i] 
-            << " ; " << name[i] 
-            << endl;
+            << " ; " << name[i];
+          outLines.push_back(outLn.str());
         }
         
+
+        outLines[(int)(addr2import / 4)] += " ; ESR - " + string(name);
+
         delete [] name;
       }
     }
-    else cout << MIPS::disasm(word) << endl;
+    else {
+      string disasmLn = MIPS::disasm(word);
+      outLines.push_back(disasmLn);
 
-    memaddr += 4;
+      if ( // I know, I know, but hey, whatever works?
+        disasmLn[0] == 'l' &&
+        disasmLn[1] == 'i' &&
+        disasmLn[2] == 's'
+      ) {
+        uint32_t lisVal;
+        try { lisVal = readMIPSbyte(bin); } 
+        catch (const string & msg) {
+          if (msg == "EOF") {
+            cerr
+              << "ERROR: "
+              << "Abrupt EOF after lis instruction"
+              << endl;
+            break;
+          }
+        }
+        outLn 
+          << ".word " << toHex(lisVal);
+        outLines.push_back(outLn.str());
+      }
+    }
   }
+
+  for (auto it = outLines.begin(); it != outLines.end(); it++)
+    cout << *it << endl;
 
   return 0;
 }
